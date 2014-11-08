0d946e3953a4e2c0b930fe087e53afceb4819689
Karim Tarek <karimmtarek@gmail.com>
2014-11-08 00:53:20 -0500

    implemented the movie edit form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'projects#index'
   get 'projects' => 'projects#index'
   get 'projects/:id' => 'projects#show', as: 'project'
+  get 'projects/:id/edit' => 'projects#edit', as: 'edit_project'
+  patch 'projects/:id' => 'projects#update'
 end
