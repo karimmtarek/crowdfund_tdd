a6e9d578d81eac9d3bacd72b356a5bcef1e74069
Karim Tarek <karimmtarek@gmail.com>
2014-11-10 17:11:55 -0500

    added a new project functionality

---
+++ spec/features/create_project_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Creating a new project" do
+
+  it "saves the project and shows the new project's details" do
+    visit projects_url
+
+    click_link 'Add New Project'
+
+    expect(current_path).to eq(new_project_path)
+
+    fill_in "Name", with: 'New Project Name'
+    fill_in "Description", with: 'Some description'
+    fill_in "Target pledge amount", with: '5000'
+    # select (Time.now.year + 1).to_s, :from => "project_pledging_ends_on_1i"
+    # fill_in "Pledging ends on", with: '2015-01-01'
+    fill_in "Website", with: 'www.xoxo.com'
+
+    click_button 'Create Project'
+
+    expect(current_path).to eq(project_path(Project.last))
+
+    expect(page).to have_text('New Project Name')
+
+  end
+
+end
